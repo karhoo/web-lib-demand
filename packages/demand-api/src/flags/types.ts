4cf69bbed1bcd8f21e3c2c3717e81a51954192e8
Yuliya Maksimchyk <yuliya.maksimchyk@karhoo.com>
2020-05-05 17:25:29 +0300

    docs(api): add v1 type for feature flags

--- packages/demand-api/src/flags/types.ts
+++ packages/demand-api/src/flags/types.ts
@@ -3,10 +3,14 @@ export type FlagsRequestPathParameters = {
   platform: string
 }
 
+// As a structure of `flags` object may change from version to version we need keep type versiongon here as well
+// See `flags` endpoint documentation on developer.karhoo.com
+type v1FlagsType = Record<string, boolean | string>
+
 export type FlagsResponse = {
   identifier: string
   version: string
-  flags: Record<string, boolean | string>
+  flags: v1FlagsType
 }
 
 export type FlagsVersionType = {
