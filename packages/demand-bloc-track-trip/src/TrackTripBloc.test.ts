e1ca256f8fdc56c2271a8eb137b458606a320bbe
miadzvedski-dzmitry <dima.medved188.tech@gmail.com>
2020-05-28 12:12:05 +0300

    feat: update name of the packages, remove unnecessary code

--- packages/demand-bloc-track-trip/src/TrackTripBloc.test.ts
+++ packages/demand-bloc-track-trip/src/TrackTripBloc.test.ts
@@ -1,15 +1,7 @@
 import { TrackTripBloc } from './TrackTripBloc'
 
 describe('TrackTripBloc', () => {
-  const quotesMock = {
-    checkAvailability: jest.fn(),
-    quotesSearch: jest.fn(),
-    quotesSearchById: jest.fn(),
-  }
-
-  describe('loadQuotes', () => {
-    it('should be a function', () => {
-      expect(typeof new TrackTripBloc(quotesMock).loadQuotes).toBe('function')
-    })
+  it('should be a function', () => {
+    expect(typeof TrackTripBloc).toBe('function')
   })
 })
