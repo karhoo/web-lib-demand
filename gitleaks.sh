29490574d828e77d99c390f97c50fb097c48b679
Hanna10Domnich <hanna.domnich@karhoo.com>
2020-05-29 17:31:59 +0300

    remove lines description

--- gitleaks.sh
+++ gitleaks.sh
@@ -7,8 +7,7 @@ if [ $gitleaksEnabled = "true" ]; then
     if [ $? -eq 1 ]; then
 cat <<\EOF
 Error: gitleaks has detected sensitive information in your changes.
-If you know what you are doing you can disable this check using:
-    git config hooks.gitleaks false
+
 EOF
 exit 1
     fi
