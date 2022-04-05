48d117f339f7d16c98b32f5033c2201e2a6f6957
Michal Ryzio <michal.ryzio@karhoo.com>
2022-04-05 15:19:03 +0200

    return to old test coverage settings

--- packages/demand-bloc-trip-create/jest.config.js
+++ packages/demand-bloc-trip-create/jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 10,
-      functions: 10,
-      lines: 10,
-      statements: 10,
+      branches: 63,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 }
